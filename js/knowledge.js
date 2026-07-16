1aa4a95ff0e88b7eac1b7c2421a9d7827581905c
Yahaya Bello <yahayadgl@gmail.com>
2026-07-16 08:36:17 +0100

    Update knowledge.js

--- js/knowledge.js
+++ js/knowledge.js
@@ -1,1 +1,48 @@
+// ===============================
+// NEXORA AI Knowledge Base V4
+// ===============================
 
+const knowledge = {
+
+    greetings: [
+        "hello",
+        "hi",
+        "salam",
+        "assalamualaikum",
+        "assalamu alaikum"
+    ],
+
+    python:
+        "Python yare ne mai sauƙin koya kuma yana da amfani wajen gina Artificial Intelligence.",
+
+    html:
+        "HTML yana gina tsarin shafin yanar gizo.",
+
+    css:
+        "CSS yana ƙawata shafin yanar gizo.",
+
+    javascript:
+        "JavaScript yana sa shafin ya zama mai motsi.",
+
+    ai:
+        "Artificial Intelligence fasaha ce da ke ba kwamfuta damar koyon bayanai da warware matsaloli.",
+
+    github:
+        "GitHub yana taimaka wa developers wajen adanawa da kuma haɗa projects.",
+
+    english:
+        "Learning English will help you understand science and AI better.",
+
+    nexora:
+        "NEXORA AI an gina ta domin taimaka wa mutane su koyi ilimi, su warware matsaloli, kuma su gina ƙwarewa.",
+
+    founder:
+        "Founder na NEXORA AI shine Yahaya Bello.",
+
+    mission:
+        "Mission: To help people learn, solve problems and build skills.",
+
+    motto:
+        "Na samu mafita. Na samu malami."
+
+};
