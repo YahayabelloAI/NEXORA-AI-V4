fcd7f59dc7e5b329459bcdf3fda34de5135f5f27
Yahaya Bello <yahayadgl@gmail.com>
2026-07-16 09:03:54 +0100

    Update memory.js

--- js/memory.js
+++ js/memory.js
@@ -1,1 +1,57 @@
+// ===============================
+// NEXORA AI Memory System V4
+// ===============================
 
+// Save Chat
+function saveChat() {
+
+    localStorage.setItem(
+        "nexora_chat",
+        JSON.stringify(chatHistory)
+    );
+
+}
+
+// Load Chat
+function loadChat() {
+
+    const saved = localStorage.getItem("nexora_chat");
+
+    if (saved) {
+
+        chatHistory = JSON.parse(saved);
+
+        renderChat();
+
+    } else {
+
+        clearChat();
+
+    }
+
+}
+
+// Clear Chat
+function clearChat() {
+
+    chatHistory = [];
+
+    localStorage.removeItem("nexora_chat");
+
+    document.getElementById("chat").innerHTML = `
+        <div class="ai-message">
+
+            <strong>NEXORA AI</strong><br>
+
+            Assalamu Alaikum.<br>
+
+            Barka da zuwa Day One.<br><br>
+
+            Na samu mafita.<br>
+
+            Na samu malami.
+
+        </div>
+    `;
+
+}
